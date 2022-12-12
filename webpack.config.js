4c7cd27b63fe0e8f78c7c83451f770547d874ca9
Stanislav Simakov <stanislav.sima@yandex.ru>
2022-12-12 04:06:29 +0300

    clean

--- webpack.config.js
+++ webpack.config.js
@@ -4,46 +4,37 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, "src", "pages","index.js"),
+  entry: path.resolve(__dirname, 'src', 'scripts', 'pages', 'index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: "[name].[contenthash].js", 
-    clean: true,
+    filename: 'main.js',
+    publicPath: '',
   },
-  devtool: "inline-source-map", 
+  mode: 'development',
+  devServer: {
+    static: path.resolve(__dirname, './dist'),
+    open: true,
+    compress: true,
+    port: 8080
+  },
   module: {
     rules: [{
-        test: /\.js$/,
-        exclude: '/node_modules/',
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-          },
-        },
-      },
+      test: /\.js$/,
+      use: 'babel-loader',
+      exclude: '/node_modules/'
+    },
       {
         test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
         type: 'asset/resource',
-        generator: {
-          filename: "images/[name].[hash][ext]"
-        }
-      },
-      {
-        test: /\.(woff(2)?|eot|ttf|otf)$/,
-        type: "asset/resource",
-        generator: {
-          filename: "fonts/[name].[hash][ext]"
-        }
       },
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1
-            }
-          },
+          loader: 'css-loader',
+          options: {
+            importLoaders: 1
+          }
+        },
           'postcss-loader'
         ]
       },
